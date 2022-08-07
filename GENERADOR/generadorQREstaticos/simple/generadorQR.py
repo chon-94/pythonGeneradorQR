82b37047f3ce8cb6c914d3a02585a935aceb4047
chon-94 <107455342+chon-94@users.noreply.github.com>
2022-08-06 22:10:52 -0500

    estatico

--- GENERADOR/generadorQREstaticos/simple/generadorQR.py
+++ GENERADOR/generadorQREstaticos/simple/generadorQR.py
@@ -5,9 +5,9 @@ cadena = input("Introduzca el texto para el codigo QR: ")
 imagen = qrcode.make(cadena)
 
 nombre_imagen  = input("Introducir el nombre de la imagen: ") + '.png'
-archivo_imagen = open('GENERADOR/generadorqr0/codigosQR/'+nombre_imagen,'wb')
+archivo_imagen = open('GENERADOR/generadorQREstaticos/simple/codigosQR/'+nombre_imagen,'wb')
 imagen.save(archivo_imagen)
 archivo_imagen.close()
 
-ruta_imagen = 'GENERADOR/generadorqr0/codigosQR/'+nombre_imagen
+ruta_imagen = 'GENERADOR/generadorQREstaticos/simple/codigosQR/'+nombre_imagen
 Image.open(ruta_imagen).show()
